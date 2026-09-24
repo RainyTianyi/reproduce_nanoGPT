75bb263964f7ad09c1548f7c6323b246a5154e0c
Tianyi_desu <1311387637@qq.com>
2026-09-24 21:31:01 +0800

    Section2: 加速。添加训练时打印Batch计算时间。改为使用TF32计算矩阵乘法。此时4060一个Batch需要约4s。

--- train_gpt2.py
+++ train_gpt2.py
@@ -243,6 +243,8 @@ class DataLoaderLite:
         return x, y
     
 # -----------------------------------------------------------------------------
+import time
+
 # 自动检测设备
 device = "cpu"
 if torch.cuda.is_available():
@@ -260,18 +262,29 @@ model = GPT(GPTConfig())
 model.to(device)
 
 # 创建数据加载器实例
-train_loader = DataLoaderLite(B=4, T=32)
+train_loader = DataLoaderLite(B=4, T=1024)
+
+# 在矩阵乘法运算中，使用 TF32(19bit) 代替 FP32(32bit)，以精读换速度和显存
+torch.set_float32_matmul_precision('high')
 
 # 使用优化器进行模型训练
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    t0 = time.time()
+    
     x, y  = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}, loss: {loss.item()}")
+    
+    torch.cuda.synchronize()    # 等待 GPU 计算完成
+    t1 = time.time()
+    dt = (t1 - t0) * 1000
+    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
+    
 import sys; sys.exit(0)
 
 # 使用训练好的模型进行预测
